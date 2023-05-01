7003ce5ddf7d1cbba6824a63165c7a7f5b140a38
FTXTY <999tylercunningham999@gmail.com>
2023-04-30 20:33:28 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import json
+import openai
+from flask import Flask, render_template, request, jsonify
+import random
+
+app = Flask(__name__)
+
+def get_openai_api_key():
+    with open('openai_secrets.json') as f:
+        secrets = json.load(f)
+    return secrets["api_key"]
+
+openai.api_key = get_openai_api_key()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get-response', methods=['POST'])
+def get_response():
+    data = request.get_json()
+    user_input = data['input']
+    prompt = f"Imagine you are an empathetic and knowledgeable therapist. Please provide a helpful, actionable, and relevant response to the following statement or question, without suggesting to seek professional help: '{user_input}'"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.4,
+        max_tokens=125,
+        top_p=1,
+        frequency_penalty=0.6,
+        presence_penalty=0
+    )
+
+    chosen_name = "Dr. Cunningham"
+
+    return jsonify({
+        'response': response.choices[0].text.strip(),
+        'name': chosen_name
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
